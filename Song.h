5adeabbf243174ae22b84b1091110e8485a423ff
Danilk0 <moskalyukdaniil@yandex.by>
2021-05-21 22:20:12 +0300

    Структура плеера

--- Song.h
+++ Song.h
@@ -9,6 +9,8 @@ namespace Eng {
 	using namespace System::Data;
 	using namespace System::Drawing;
 	using namespace System::Media;
+	using namespace System::Data::OleDb;
+
 	/// <summary>
 	/// Сводка для Song
 	/// </summary>
@@ -42,7 +44,8 @@ namespace Eng {
 	private: System::Windows::Forms::Button^ buttonGrammar;
 	private: System::Windows::Forms::Button^ button3;
 	private: System::Windows::Forms::Button^ button2;
-	private: System::Windows::Forms::Button^ button1;
+
+	private: System::Windows::Forms::Label^ label1;
 
 	protected:
 
@@ -63,9 +66,9 @@ namespace Eng {
 		{
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(Song::typeid));
 			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->button3 = (gcnew System::Windows::Forms::Button());
 			this->button2 = (gcnew System::Windows::Forms::Button());
-			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->buttonGrammar = (gcnew System::Windows::Forms::Button());
 			this->panel1->SuspendLayout();
 			this->SuspendLayout();
@@ -74,16 +77,27 @@ namespace Eng {
 			// 
 			this->panel1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(50)), static_cast<System::Int32>(static_cast<System::Byte>(52)),
 				static_cast<System::Int32>(static_cast<System::Byte>(77)));
+			this->panel1->Controls->Add(this->label1);
 			this->panel1->Controls->Add(this->button3);
 			this->panel1->Controls->Add(this->button2);
-			this->panel1->Controls->Add(this->button1);
 			this->panel1->Controls->Add(this->buttonGrammar);
 			this->panel1->Dock = System::Windows::Forms::DockStyle::Bottom;
+			this->panel1->ForeColor = System::Drawing::Color::Gainsboro;
 			this->panel1->Location = System::Drawing::Point(0, 717);
 			this->panel1->Name = L"panel1";
 			this->panel1->Size = System::Drawing::Size(1178, 132);
 			this->panel1->TabIndex = 0;
 			this->panel1->Paint += gcnew System::Windows::Forms::PaintEventHandler(this, &Song::panel1_Paint);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 13.8F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label1->Location = System::Drawing::Point(520, 64);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(0, 29);
+			this->label1->TabIndex = 5;
 			// 
 			// button3
 			// 
@@ -110,28 +124,12 @@ namespace Eng {
 			this->button2->ForeColor = System::Drawing::SystemColors::ControlLightLight;
 			this->button2->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button2.Image")));
 			this->button2->ImageAlign = System::Drawing::ContentAlignment::MiddleLeft;
-			this->button2->Location = System::Drawing::Point(401, 39);
+			this->button2->Location = System::Drawing::Point(305, 39);
 			this->button2->Name = L"button2";
 			this->button2->Size = System::Drawing::Size(90, 80);
 			this->button2->TabIndex = 3;
 			this->button2->UseVisualStyleBackColor = false;
 			this->button2->Click += gcnew System::EventHandler(this, &Song::button2_Click);
-			// 
-			// button1
-			// 
-			this->button1->FlatAppearance->BorderSize = 0;
-			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
-			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(204)));
-			this->button1->ForeColor = System::Drawing::SystemColors::ControlLightLight;
-			this->button1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button1.Image")));
-			this->button1->ImageAlign = System::Drawing::ContentAlignment::MiddleLeft;
-			this->button1->Location = System::Drawing::Point(305, 40);
-			this->button1->Name = L"button1";
-			this->button1->Size = System::Drawing::Size(90, 80);
-			this->button1->TabIndex = 2;
-			this->button1->UseVisualStyleBackColor = false;
-			this->button1->Click += gcnew System::EventHandler(this, &Song::button1_Click);
 			// 
 			// buttonGrammar
 			// 
@@ -163,30 +161,65 @@ namespace Eng {
 			this->Text = L"Song";
 			this->Load += gcnew System::EventHandler(this, &Song::Song_Load);
 			this->panel1->ResumeLayout(false);
+			this->panel1->PerformLayout();
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
 	
 		SoundPlayer^ player = nullptr;
+		String^ SongChange(int id) {
+			String^ connectionString = "provider=Microsoft.Jet.OLEDB.4.0;Data Source=Database1.mdb";
+			OleDbConnection^ dbConnection = gcnew OleDbConnection(connectionString);
+			dbConnection->Open();
+			String^ query1 = "SELECT SongName, SongWay  FROM [table_name] WHERE Код = " + id;
+			OleDbCommand^ dbCommand1 = gcnew OleDbCommand(query1, dbConnection);
+			OleDbDataReader^ dbReader = dbCommand1->ExecuteReader();
+			while (dbReader->Read()) {
+				label1->Text = Convert::ToString(dbReader["SongName"]);
+				return Convert::ToString(dbReader["SongWay"]);
+			}
+			dbReader->Close();
+			dbConnection->Close();
+		}
 			//gcnew SoundPlayer("D:/VS_2019/LastDance/Media/aplodismentyi-38722.wav");
-
+		int id = 1;
 	private: System::Void Song_Load(System::Object^ sender, System::EventArgs^ e) {
 		player = gcnew SoundPlayer();
-		
+		player->SoundLocation = SongChange(id);
+		id++;
 	}
 
-
+		   bool flag = true;
 private: System::Void buttonGrammar_Click(System::Object^ sender, System::EventArgs^ e) {
+	if (flag) {
+		player->Play();
+		buttonGrammar->Image = Image::FromFile("C:/Users/winda/source/repos/01thn/Eng/Media/стоп.png");
+		flag = false;
+	}
+	else {
+		player->Stop();
+		buttonGrammar->Image = Image::FromFile("C:/Users/winda/source/repos/01thn/Eng/Media/старт.png");
+		flag = true;
+	}
+	
 }
-private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
 
-}
 private: System::Void panel1_Paint(System::Object^ sender, System::Windows::Forms::PaintEventArgs^ e) {
 }
 private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+	buttonGrammar->Image = Image::FromFile("C:/Users/winda/source/repos/01thn/Eng/Media/старт.png");
+	player->Stop();
+	player->SoundLocation = SongChange(id);
+	id++;
+	flag = true;
 }
 private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) {
+	id--;
+	player->Stop();
+	player->SoundLocation = SongChange(id);
+	buttonGrammar->Image = Image::FromFile("C:/Users/winda/source/repos/01thn/Eng/Media/старт.png");
+	flag = true;
 }
 };
 }
