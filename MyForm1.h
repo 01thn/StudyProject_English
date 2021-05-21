39daa14fb747b4914687df47aa798f079a990a91
01thn <01thn@github.com>
2021-05-21 19:53:57 +0300

    Паст

--- MyForm1.h
+++ MyForm1.h
@@ -34,6 +34,10 @@ namespace Eng {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Label^ label1;
+	protected:
+
+	protected:
 
 	protected:
 
@@ -50,7 +54,18 @@ namespace Eng {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(68, 65);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(1069, 17);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Время Past Simple используется для обозначения действия, которое произошло в опре"
+				L"деленное время в прошлом и время совершения которого уже истекло.";
 			// 
 			// MyForm1
 			// 
@@ -58,10 +73,12 @@ namespace Eng {
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::Color::White;
 			this->ClientSize = System::Drawing::Size(1178, 849);
+			this->Controls->Add(this->label1);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
 			this->Name = L"MyForm1";
 			this->Text = L"MyForm1";
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
