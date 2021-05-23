80e618d3e8ce44e4089321f40dc5658bb5983971
01thn <01thn@github.com>
2021-05-23 18:31:13 +0300

    пазл2

--- MyForm.cpp
+++ MyForm.cpp
@@ -1,5 +1,16 @@
 #include "MyForm.h"
+#include"Song.h"
+
 #include <Windows.h>
+namespace Eng
+{
+	System::Void MyForm::SongClose() {
+		Song^ f = gcnew Song();
+		f->Hide();
+		f->playerStop();
+	}
+
+}
 using namespace Eng; // Название проекта
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Application::EnableVisualStyles();
